92d08ae4ab6b03e3cd2cee5bf9eaa01299a425ec
Logan Bontrager <logan.bontrager@orchard.com>
2023-02-22 17:12:38 -0500

    improve nvimtree & keymap nits

--- nvim/.config/nvim/lua/setup/keymaps.lua
+++ nvim/.config/nvim/lua/setup/keymaps.lua
@@ -13,8 +13,8 @@ vim.g.mapleader = ' '
 -- NORMAL --
 map('n', '<leader>o', ':on<CR>') -- close other buffers
 map('n', '<leader>vm', ':vsp $MYVIMRC<CR>') -- open config
-map('n', '<leader>d', ':NvimTreeToggle<cr>')
-map('n', '<leader>cd', ':NvimTreeFindFile<cr>')
+map('n', '<leader>d', ':NvimTreeToggle<CR>')
+map('n', '<leader>cd', ':NvimTreeFindFile<CR>')
 map('n', '<leader>j', '<C-W>h') -- dirty left window movement
 map('n', '<leader>l', '<C-W>l')
 
@@ -23,13 +23,13 @@ map('n', '<leader>gs', ':G<CR>')
 
 -- telescope
 map('n', '<leader>ff', ":lua require('telescope.builtin').find_files()<CR>")
-map('n', '<leader>fb', ":lua require('telescope.builtin').buffers()<CR>")
+map('n', '<leader>b', ":lua require('telescope.builtin').buffers()<CR>")
 map('n', '<leader>fg', ":lua require('telescope.builtin').live_grep()<CR>")
 map('n', '<leader>gb', ":lua require('telescope.builtin').git_branches()<CR>")
 
 -- preview
 map('n', 'gd', ":lua require('goto-preview').goto_preview_definition()<CR>", { noremap=true }) -- get definition in preview
-map('n', '<leader>q', ":lua require('goto-preview').close_all_win()<CR>", { noremap=true }) -- close all definition windows
+map('n', '<leader>q', ":lua require('goto-preview').close_all_win()<CR> || :NvimTreeClose<CR>", { noremap=true }) -- close all windows
 map('n', 'gr', ":lua require('goto-preview').goto_preview_references()<CR>", { noremap=true }) -- get references (uses telescope)
 
 -- INSERT --
--- nvim/.config/nvim/lua/setup/nvim-tree.lua
+++ nvim/.config/nvim/lua/setup/nvim-tree.lua
@@ -1,61 +1,105 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 local lib = require('nvim-tree.lib')
 local view = require('nvim-tree.view')
 local open = require('nvim-tree.actions.node.open-file')
 
 local function collapse_all()
-  require('nvim-tree.actions.collapse-all').fn()
+  require("nvim-tree.actions.tree-modifiers.collapse-all").fn()
 end
 
-local function edit_or_open()
-  -- open as vsplit on current node
-  local action = 'edit'
-  local node = lib.get_node_at_cursor()
-
-  -- Just copy what's done normally with vsplit
-  if node.link_to and not node.nodes then
-    open.fn(action, node.link_to)
-    view.close() -- Close the tree if file was opened
-  elseif node.nodes ~= nil then
-    lib.expand_or_collapse(node)
-  else
-    open.fn(action, node.absolute_path)
-    view.close() -- Close the tree if file was opened
+local function partial(f, x)
+  return function(...)
+    return f(x, ...)
   end
 end
 
-local function vsplit_preview()
-  -- open as vsplit on current node
-  local action = 'vsplit'
+local function open_node(action)
   local node = lib.get_node_at_cursor()
 
-  -- Just copy what's done normally with vsplit
   if node.link_to and not node.nodes then
-    open.fn(action, node.link_to)
+      open.fn(action, node.link_to)
+      view.close()
   elseif node.nodes ~= nil then
-    lib.expand_or_collapse(node)
+      lib.expand_or_collapse(node)
   else
-    open.fn(action, node.absolute_path)
+      open.fn(action, node.absolute_path)
+      view.close()
   end
-
-  -- Finally refocus on tree if it was lost
-  view.focus()
 end
 
+local function open_nvim_tree(data)
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not no_name and not directory then
+    return
+  end
+
+  -- change to the directory
+  if directory then
+    vim.cmd.cd(data.file)
+  end
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+local HEIGHT_RATIO = 0.8  -- You can change this
+local WIDTH_RATIO = 0.5   -- You can change this too
 require('nvim-tree').setup({
+  hijack_cursor = true,
+  update_cwd = false,
+  reload_on_bufenter = true,
   view = {
     mappings = {
       custom_only = false,
       list = {
-        { key = 'l', action = 'edit', action_cb = edit_or_open },
-        { key = 'L', action = 'vsplit_preview', action_cb = vsplit_preview },
+        { key = 'l', action = 'edit', action_cb = partial(open_node, 'edit') },
+        { key = 'L', action = 'vsplit_preview', action_cb = partial(open_node, 'vsplit') },
         { key = 'h', action = 'close_node' },
         { key = 'H', action = 'collapse_all', action_cb = collapse_all }
       }
     },
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = 'rounded',
+          relative = 'editor',
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
   },
   actions = {
     open_file = {
       quit_on_open = false
     }
-  }
+  },
+  update_focused_file = {
+    enable = true,
+    update_root = true,
+  },
 })
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
